151436d28988e4194c6ebf79a0cb87eb6782675b
krishnay <krishnatejay@Admins-MacBook-Pro.local>
2021-09-15 23:19:39 +0530

    Add union operatio

---
+++ mdds/src/main/java/mdds/MastHeadListDataFetcher.java
@@ -0,0 +1,25 @@
+package mdds;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import graphql.com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+@DgsComponent
+public class MastHeadDataFetcher {
+
+    @DgsQuery
+    public MastHead mastHead(@InputArgument String pid) {
+        if("1".equals(pid)) {
+            List<ContentItem> contentItems = ImmutableList.of(new ContentItem(1), new ContentItem(2));
+            List<AdvtItem> advtItems = ImmutableList.of(new AdvtItem(1), new AdvtItem(2));
+            return new MastHead(contentItems, advtItems);
+        } else{
+            List<ContentItem> contentItems = ImmutableList.of(new ContentItem(3), new ContentItem(4));
+            List<AdvtItem> advtItems = ImmutableList.of(new AdvtItem(3), new AdvtItem(4));
+            return new MastHead(contentItems, advtItems);
+        }
+    }
+}
